36c2f83b3a7232c84fa32f50c1ead2b43ba82b71
Alberto Bautista <1921095+albertoide@users.noreply.github.com>
2022-12-21 23:05:28 +0100

    update

--- nvim/.config/nvim/lua/user/options.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.nu = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.showmode = false
-vim.opt.swapfile = false
-vim.opt.wrap = false
-vim.opt.termguicolors = true
-vim.opt.listchars="tab:»·,eol:↲,nbsp:␣,trail:✗,space:·"
